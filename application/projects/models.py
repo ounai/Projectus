ee981f3f4dc6eadbf68b0db1c69465881976bf71
Onni Rantanen <onni.rantanen@helsinki.fi>
2019-04-04 21:41:35 +0300

    overview of things due today

--- application/projects/models.py
+++ application/projects/models.py
@@ -1,6 +1,8 @@
 from application import db
 from application.models import Base
 
+from sqlalchemy.sql import text
+
 class Project(Base):
     name = db.Column(db.String(144), nullable = False)
     complete = db.Column(db.Boolean, nullable = False)
@@ -13,6 +15,24 @@ class Project(Base):
     def __init__(self, name):
         self.name = name
         self.complete = False
+
+    @staticmethod
+    def find_projects_by_user_with_tasks_due_on(account_id, deadline):
+        stmt = text("SELECT Project.id, Project.name FROM Project"
+                    " LEFT JOIN Task on Task.project_id = Project.id"
+                    " WHERE (Task.deadline = :deadline AND Project.account_id = :account_id AND Task.complete = 0)"
+                    " GROUP BY Project.id").params(deadline = deadline, account_id = account_id)
+
+        res = db.engine.execute(stmt)
+
+        result = []
+        for row in res:
+            result.append({
+                "id": row[0],
+                "name": row[1]
+            })
+
+        return result
 
 class Task(Base):
     name = db.Column(db.String(144), nullable = False)
--- application/views.py
+++ application/views.py
@@ -1,7 +1,20 @@
 from flask import render_template
+from flask_login import current_user
+
+from datetime import date
+
 from application import app
+from application.projects.models import Project, Task
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if current_user.is_authenticated:
+        today_date = date.today()
+
+        projects_due_today = Project.query.filter_by(account_id = current_user.id, complete = False, deadline = today_date)
+        projects_with_tasks_due_today = Project.find_projects_by_user_with_tasks_due_on(current_user.id, today_date)
+
+        return render_template("index.html", projects_due_today = projects_due_today, projects_with_tasks_due_today = projects_with_tasks_due_today)
+    else:
+        return render_template("index.html")
 
